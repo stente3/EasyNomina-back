b310dd0814fd1553b7cf5e6a8d2a5207943fd160
Stente3 <stentem0103@gmail.com>
2024-12-11 14:44:20 -0500

    Se añadió más especificación en los mensajes de error de la autenticación

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -4,11 +4,17 @@ import { TOKEN_SECRET } from "../config.js";
 // middleware to check if token is valid
 export const authRequired = (req, res, next) => {
   const { token } = req.cookies;
-  if (!token) return res.status(401).json({ message: "Unauthorized" }); // if token is not present in cookies, return unauthorized
+  if (!token)
+    return res
+      .status(401)
+      .json({ message: "Unauthorized, you dont have any token" }); // if token is not set, return unauthorized
 
   // verify token
   jwt.verify(token, TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.status(401).json({ message: "Unauthorized" }); // if token is not valid, return unauthorized
+    if (err)
+      return res
+        .status(401)
+        .json({ message: "Unauthorized, your token is invalid" }); // if token is invalid, return unauthorized
 
     req.user = decoded; // set user in request
 
